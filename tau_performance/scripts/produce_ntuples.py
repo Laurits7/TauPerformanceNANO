9b22f061933b243dc3b12b5536237cf91e6b2187
Laurits <laurits.tani@gmail.com>
2022-03-25 14:00:26 +0200

    fake matching improvement and plotting

--- tau_performance/scripts/produce_ntuples.py
+++ tau_performance/scripts/produce_ntuples.py
@@ -9,20 +9,30 @@ from tau_performance.tools import general
 @hydra.main(config_path='../config', config_name='config')
 def main(cfg: DictConfig) -> None:
     os.makedirs(cfg.output_dir, exist_ok=True)
-    for sample_type in ['QCD', 'ggH_htt']:
-        print("Started loading signal events")
-        events = general.load_events(
-                                    file_path=cfg.eff_file.path,
-                                    tree_name=cfg.eff_file.tree)
-        print("Events loaded, filling ntuples for signal")
-        npro.fill_eff_ntuple(events, sample_type, cfg)
-        print("Finished filling ntuples for signal, loading events for fakes")
-        events = general.load_events(
-                                    file_path=cfg.fakes_file.path,
-                                    tree_name=cfg.fakes_file.tree)
-        print("Events loaded, filling ntuples for fakes")
-        npro.fill_all_fake_ntuples(events, sample_type, cfg)
-        print("Fakes ntuple filled")
+    signal_info = cfg.eff_file
+    background_info = cfg.fakes_file
+    print(f"Started loading signal ({signal_info.sample_name}) "
+        f"events from {signal_info.path}")
+    signal_events = general.load_events(
+                                file_path=signal_info.path,
+                                tree_name=signal_info.tree)
+    print("Events loaded, filling ntuples for signal")
+    npro.fill_ref_obj_ntuple(
+                        signal_events, cfg.genTau, signal_info.sample_name, cfg)
+    npro.fill_ref_obj_ntuple(
+                signal_events, cfg.fakes.recoJet, signal_info.sample_name, cfg)
+    print("Finished filling ntuples for background.")
+    print(f"Loading events for fakes ({background_info.sample_name}) "
+        f"events from {background_info.path}")
+    background_events = general.load_events(
+                                file_path=background_info.path,
+                                tree_name=background_info.tree)
+    print("Events loaded, filling ntuples for fakes")
+    npro.fill_ref_obj_ntuple(
+                background_events,  cfg.genTau, background_info.sample_name, cfg)
+    npro.fill_ref_obj_ntuple(
+         background_events, cfg.fakes.recoJet, background_info.sample_name, cfg)
+    print("Fakes ntuple filled")
 
 
 if __name__ == '__main__':
--- tau_performance/scripts/produce_plots.py
+++ tau_performance/scripts/produce_plots.py
@@ -14,38 +14,39 @@ import os
 @hydra.main(config_path='../config', config_name='config')
 def main(cfg: DictConfig) -> None:
     os.makedirs(cfg.output_dir, exist_ok=True)
-    # dm.decay_mode_reconstruction(cfg)
+
+    ################### DM RECONSTRUCTION ###################
+    dm.decay_mode_reconstruction(cfg)
 
 
-    # ####################
-    eff = Efficiency('ggH_htt', cfg.genTau, cfg)
-    efficiencies = eff.efficiencies
-    eff_reco_histos = eff.reco_histos
-    eff_obj = cfg.genTau
-    pef.plot_efficiencies(efficiencies, 'eff', eff_obj, cfg)
-    pef.plot_eff_reco_histos(eff_reco_histos, 'eff', cfg)
+    # ################### EFFICIENCY ###################
+    # eff = Efficiency('ggH_htt', cfg.genTau, cfg)
+    # efficiencies = eff.efficiencies
+    # eff_reco_histos = eff.reco_histos
+    # eff_obj = cfg.genTau
+    # pef.plot_efficiencies(efficiencies, 'eff', eff_obj, cfg)
+    # pef.plot_eff_reco_histos(eff_reco_histos, 'eff', cfg)
 
-    fake = Efficiency('QCD', cfg.fakes.genJet, cfg)
-    fake_rates = fake.efficiencies
-    fake_reco_histos = fake.reco_histos
-    fake_obj = cfg.fakes.genJet
-    pef.plot_efficiencies(fake_rates, 'fake', fake_obj, cfg)
-    pef.plot_eff_reco_histos(fake_reco_histos, 'fake', cfg)
-    ###################
-    res_eff = Response("ggH_htt", cfg.genTau, cfg)
-    pr.plot_all_responses(res_eff.energy_response, cfg.genTau, "ggH_htt", cfg)
-    res_fake = Response("ggH_htt", cfg.fakes.genJet, cfg)
-    pr.plot_all_responses(res_fake.energy_response, cfg.fakes.genJet, "ggH_htt", cfg)
 
-    ######### ROC ##############
+    # ################### FAKE RATES ###################
+    # fake = Efficiency('QCD', cfg.fakes.recoJet, cfg)
+    # fake_rates = fake.efficiencies
+    # fake_reco_histos = fake.reco_histos
+    # fake_obj = cfg.fakes.recoJet
+    # pef.plot_efficiencies(fake_rates, 'fake', fake_obj, cfg)
+    # pef.plot_eff_reco_histos(fake_reco_histos, 'fake', cfg)
 
-    qcd_eff = Efficiency('QCD', cfg.fakes.genJet, cfg)
-    qcd_total_fakerates = qcd_eff.total_efficiencies
-    htt_total_eff = eff.total_efficiencies
 
-    print(htt_total_eff)
-    print(qcd_total_fakerates)
-    prc.plot_roc_curve(htt_total_eff, qcd_total_fakerates, cfg)
+    # ################### RESPONSE ###################
+    # res_eff = Response("ggH_htt", cfg.genTau, cfg)
+    # pr.plot_all_responses(res_eff.energy_response, cfg.genTau, "ggH_htt", cfg)
+
+
+    # ###################   ROC   ###################
+    # qcd_eff = Efficiency('QCD', cfg.fakes.recoJet, cfg)
+    # qcd_total_fakerates = qcd_eff.total_efficiencies
+    # htt_total_eff = eff.total_efficiencies
+    # prc.plot_roc_curve(htt_total_eff, qcd_total_fakerates, cfg)
 
 
 if __name__ == '__main__':
